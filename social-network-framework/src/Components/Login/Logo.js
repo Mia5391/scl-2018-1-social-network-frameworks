7df327cbf3d6987198aecf0a9990deba42e81009
Sofía Maturana <sofimatsu@gmail.com>
2018-09-15 12:31:41 -0300

    eslint json instead of js file

---
+++ social-network-framework/src/Components/Login/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const path = href('./logoSocialNetwork.png');  
+
+// const Logo = ({ path, altLogo }) => (
+//     <img src={path} alt={altLogo} className="appLogo"/>
+//   );
+  
+
+export default Logo; 
