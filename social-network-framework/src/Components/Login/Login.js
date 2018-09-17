24dd15c617c4e89f9856bd5e7dbe12206b8fbac6
Sofía Maturana <sofimatsu@gmail.com>
2018-09-17 18:05:17 -0300

    gh pages

--- social-network-framework/src/Components/Login/Login.js
+++ social-network-framework/src/Components/Login/Login.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ContainerTitle from './ContainerTitle'; 
+import { ContainerTitle } from './ContainerTitle'; 
 import SignInForm from './SignInForm';
 import './Login.css'; 
 
 class Login extends Component{
-    
     
     render(){
         return (
--- social-network-framework/src/Components/Login/Logo.js
+++ social-network-framework/src/Components/Login/Logo.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logoSocialNetwork from './logoSocialNetwork.png'; 
+import logoApp from './logoSocialNetwork.png'; 
 import './Login.css'; 
 
 const Logo = () => {
     return (
       <div>
-        <img className="logoApp" alt="logoApp" src={logoSocialNetwork} />
+        <img className="logoSocialNetwork" alt="logoApp" src={logoApp} />
       </div>
     )
   }
