3a0361a9fc9e9a63e1d85f85cc93a97fb6a0f051
Sofía Maturana <sofimatsu@gmail.com>
2018-09-26 15:51:18 -0300

    register component commented (not working)

--- social-network-framework/src/App.js
+++ social-network-framework/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login/Login';
-import Register from './Components/Register/Register';
+// import Register from './Components/Register/Register';
 import Timeline from './Components/Timeline/Timeline';
 
 
@@ -11,16 +11,16 @@ import Timeline from './Components/Timeline/Timeline';
 class App extends Component {
   render() {
     return (
-      
+     <div className= "app">
         <Router>
-          <div>
+         <div>
           <Route exact path="/" component={Login}/>
-          <Route path="/register" component={Register}/>
+          {/* <Route path="/register" component={Register}/> */}
           <Route path="/timeline" component={Timeline}/>
           </div>
         </Router>
-      
-    
+        {/* </div> */}
+        </div>
     );
   }
 }
--- social-network-framework/src/Components/Register/Register.js
+++ social-network-framework/src/Components/Register/Register.js
@@ -1,49 +1,49 @@
-import React, { Component } from 'react';
-import firebaseIntegration from '../../firebase'
-import { withRouter } from 'react-router-dom';
-import './Register.css';
+// import React, { Component } from 'react';
+// import firebaseIntegration from '../../firebase'
+// import { withRouter } from 'react-router-dom';
+// import './Register.css';
 
-class Register extends Component {
-  constructor(props) {
-    super(props);
-    this.signup = this.signup.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = {
-      email: '',
-      password: ''
-    };
-  }
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
+// class Register extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.signup = this.signup.bind(this);
+//     this.handleChange = this.handleChange.bind(this);
+//     this.state = {
+//       email: '',
+//       password: ''
+//     };
+//   }
+//   handleChange(e) {
+//     this.setState({ [e.target.name]: e.target.value });
+//   }
 
-  signup(e) {
-    e.preventDefault();
-    firebaseIntegration.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
-    }).then((u) => { console.log(u) })
-      .catch((error) => {
-        console.log(error);
-      })
-  }
+//   signup(e) {
+//     e.preventDefault();
+//     firebaseIntegration.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
+//     }).then((u) => { console.log(u) })
+//       .catch((error) => {
+//         console.log(error);
+//       })
+//   }
 
-  render() {
-    return (
-      <Row className="example">
-        <Col s={12}>
-          <Input s={12} value={this.state.email} onChange={this.handleChange} type="email" name="email" placeholder="Email">
-            <Icon>account_circle</Icon>
-          </Input>
-          <Input s={12} type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.handleChange}>
-            <Icon>lock</Icon>
-          </Input>
-          <div className="center">
-            <Button s={6} className='ButtonLarge1' waves='light' node='a' onClick={this.signup}>REGISTER</Button>
-          </div>
-        </Col>
-      </Row>
-    )
-  }
+//   render() {
+//     return (
+//       <Row className="example">
+//         <Col s={12}>
+//           <Input s={12} value={this.state.email} onChange={this.handleChange} type="email" name="email" placeholder="Email">
+//             <Icon>account_circle</Icon>
+//           </Input>
+//           <Input s={12} type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.handleChange}>
+//             <Icon>lock</Icon>
+//           </Input>
+//           <div className="center">
+//             <Button s={6} className='ButtonLarge1' waves='light' node='a' onClick={this.signup}>REGISTER</Button>
+//           </div>
+//         </Col>
+//       </Row>
+//     )
+//   }
 
-}
+// }
 
-export default withRouter(Register); 
+// export default withRouter(Register); 
