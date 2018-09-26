9f0116d2187e29bf270c4656946fd5942265b062
Sofía Maturana <sofimatsu@gmail.com>
2018-09-26 15:51:48 -0300

    change of classname of row

--- social-network-framework/src/Components/Login/SignInForm.js
+++ social-network-framework/src/Components/Login/SignInForm.js
@@ -28,13 +28,13 @@ constructor(props) {
  
   render() {
     return ( 
-    <Row className="example">
+    <Row className="signInForm">
     <Col s={12}>
       <Input s={12} value={this.state.email} onChange={this.handleChange} type="email" name="email"  placeholder="Email">
-        <Icon>account_circle</Icon>
+        <Icon></Icon>
       </Input>
       <Input s={12} type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.handleChange}>
-        <Icon>lock</Icon>
+        <Icon></Icon>
       </Input>
       <div className="center">
       <Button s={6} className='ButtonLarge1' waves='light' node='a' href='' type="submit" onClick={this.login}> Login </Button>
